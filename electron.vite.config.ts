27c22f00082802c9f39c5ebb4a1a7e4bd684c908
xuwang <xuwang@subaonet.com>
2024-04-19 13:57:11 +0800

    添加ant-design-vue auto-import

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,11 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
+import styleImport, { AndDesignVueResolve } from 'vite-plugin-style-import'
 
 export default defineConfig({
   main: {
@@ -15,7 +20,42 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        imports: ['vue', 'vue-router'],
+        resolvers: [AntDesignVueResolver()],
+        dts: 'src/types/auto-imports.d.ts',
+        eslintrc: {
+          enabled: false, // 默认false, true启用。生成一次就可以，避免每次工程启动都生成，一旦生成配置文件之后，最好把enable关掉，即改成false //否则这个文件每次会在重新加载的时候重新生成，这会导致eslint有时会找不到这个文件。当需要更新配置文件的时候，再重新打开
+          filepath: './.eslintrc-auto-import.json', // 生成json文件,可以不配置该项，默认就是将生成在根目录
+          globalsPropValue: true
+        }
+      }),
+      Components({
+        resolvers: [
+          AntDesignVueResolver({
+            importStyle: false,
+            resolveIcons: true
+          })
+        ],
+        dts: 'src/types/components.d.ts'
+      }),
+      styleImport({
+        resolves: [AndDesignVueResolve()],
+        libs: [
+          {
+            libraryName: 'ant-design-vue',
+            esModule: true,
+            resolveStyle: (name) => {
+              return `ant-design-vue/es/${name}/style/index`
+            }
+          }
+        ]
+      })
+    ],
+
     //配置css变量
     css: {
       preprocessorOptions: {
--- src/renderer/src/views/home/index.vue
+++ src/renderer/src/views/home/index.vue
@@ -1,3 +1,10 @@
 <template>
-  <div>Home Page <router-link to="/test">Go Test</router-link></div>
+  <div>
+    Home Page <router-link to="/test">Go Test</router-link>
+    <a-button type="primary">Primary Button</a-button>
+  </div>
 </template>
+<script setup lang="ts">
+const a = ref(3)
+console.log(a.value)
+</script>
